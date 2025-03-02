da28f45ef43101bf6e85ea714851b830b391e43f
Justmek <vocotnhan@gmail.com>
2025-03-02 08:58:02 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,7 +3,7 @@
 # Save this as restart_node.sh
 while true; do
     echo "Starting Node.js application..."
-    node /path/to/your/app.js
+    node app.js
     echo "Application crashed or stopped. Restarting in 5 seconds..."
     sleep 5
 done
